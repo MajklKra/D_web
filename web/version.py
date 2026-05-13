2b2536c6aa967a887f6e6d8e08bdb36a106974cb
Michal <Kraninger.M@seznam.cz>
2026-05-13 14:49:53 +0200

    Left menu made responsive #1

--- web/version.py
+++ web/version.py
@@ -1,9 +1,9 @@
-__version__ = "1.0.60"
+__version__ = "1.0.61"
 
-# První řádek left-menu navržen jako responsivní
+# Left-menu responsivní
 
 # Jedná se o tuto produkční verzi č. ----
 
 # Vytvořil  Michal Kraninger
-# Datum:    12.5.2026
+# Datum:    13.5.2026
 
