26428437a7e52b1ec13f90762eb7a77bd7522924
Michal <Kraninger.M@seznam.cz>
2026-05-22 13:27:11 +0200

    LeftMenu  and navbar made responsive

--- web/version.py
+++ web/version.py
@@ -1,9 +1,11 @@
-__version__ = "1.1.9"
+__version__ = "1.1.12"
 
-# Upravené menu do 992px
+# Left-menu made responsive
 
 # Jedná se o tuto produkční verzi č. ----
 
 # Vytvořil  Michal Kraninger
 # Datum:    22.5.2026
+
+# Verze pro prezentaci
 
