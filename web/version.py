58071ae5cecd1892323fcb5ff0278b42dcb7224e
Michal <Kraninger.M@seznam.cz>
2026-05-22 08:45:25 +0200

    Left-menu berakpoint 1399px corrected

--- web/version.py
+++ web/version.py
@@ -1,9 +1,9 @@
-__version__ = "1.1.6"
+__version__ = "1.1.8"
 
-# Upravené menu pro do 1920px
+# Upravené menu do 1399px
 
 # Jedná se o tuto produkční verzi č. ----
 
 # Vytvořil  Michal Kraninger
-# Datum:    21.5.2026
+# Datum:    22.5.2026
 
