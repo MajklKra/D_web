189f53b429f8d35a016900bd2d7979eeceb45bf9
Michal <Kraninger.M@seznam.cz>
2026-06-03 15:29:19 +0200

    DBC-row3-occupC-c2-empC-c1 2 rows added

--- web/version.py
+++ web/version.py
@@ -1,11 +1,11 @@
 
-__version__ = "1.1.45"
+__version__ = "1.1.46"
 
-# DBC-row3-occupC-c2-empC-c1 newly created
+# DBC-row3-occupC-c2-empC-c1 newly created 2 rows added
 
 # Jedná se o tuto produkční verzi č. ----
 
-# Vytvořil  Michal Kraninger
+# Vytvořil:  Michal Kraninger
 # Datum:    3. 6. 2026
 
 
