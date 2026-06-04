4d00211110b5b12ebbed21bb34f39d351b2abfa4
Michal <Kraninger.M@seznam.cz>
2026-06-04 12:30:59 +0200

    # DBC-row3-occupC-c2-empC-c1 newly third row has been added

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.1.48"
+__version__ = "1.1.49"
 
-# DBC-row3-occupC-c2-empC-c1 newly second row has been added
+# DBC-row3-occupC-c2-empC-c1 newly third row has been added
 
 # Jedná se o tuto produkční verzi č. ----
 
