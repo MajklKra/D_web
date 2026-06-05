8b78bb58b6ee1d64006e61b48f298ad3bc1900b0
Michal <Kraninger.M@seznam.cz>
2026-06-05 13:05:54 +0200

    Dashboard first not resposive draft

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.1.56"
+__version__ = "1.1.58"
 
-# DBC-row3-lastC-capC4 - first version created
+# Dashboard first not resposive draft
 
 # Jedná se o tuto produkční verzi č. ----
 
