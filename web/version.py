b16b00273b4e1b779a70b20c2d212c8f8818f842
Michal <Kraninger.M@seznam.cz>
2026-06-17 09:10:49 +0200

    DBC-row2-patientC from 992px first draft.

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.1.96"
+__version__ = "1.1.97"
 
-# DBC-row2-patientC from 1200px first draft.
+# DBC-row2-patientC from 992px first draft.
 
 # Jedná se o tuto produkční verzi č. ----
 
