77af168d8c0f41056c67870303c7640342af509e
Michal <Kraninger.M@seznam.cz>
2026-06-19 11:31:26 +0200

    DBC-row2-capC2 from 1200px made responsive

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.2.33"
+__version__ = "1.2.34"
 
-# DBC-row2-capC2 from 1400px made responsive
+# DBC-row2-capC2 from 1200px made responsive
 
 # Jedná se o tuto produkční verzi č. ----
 
