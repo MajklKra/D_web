c7de2e64032cb48301fb0e19cce9fd535f93c31e
Michal <Kraninger.M@seznam.cz>
2026-06-23 13:10:15 +0200

    DBC-row2-capC3 1200px made responsive without chart

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.2.44"
+__version__ = "1.2.45"
 
-# DBC-row2-capC3 1200px responsive without chart
+# DBC-row2-capC3 1200px made responsive without chart
 
 # Jedná se o tuto produkční verzi č. ----
 
