7a94794fcfb8b66a60e846037bfefea808a120eb
Michal <Kraninger.M@seznam.cz>
2026-06-26 12:58:55 +0200

    DBC-row3-employeesC  1400px scroll-bar made functional

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
 __version__ = "1.2.43"
 
-# DBC-row3-employeesC ... dots added to the project
+# DBC-row3-employeesC  1400px scroll-bar made functional
 
 # Jedná se o tuto produkční verzi č. ----
 
