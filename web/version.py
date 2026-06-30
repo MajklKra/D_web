15940ad0962d438223daecee1284a22abd5b443d
Michal <Kraninger.M@seznam.cz>
2026-06-30 09:37:34 +0200

    DBC-row3-occupC 992px - DBC-row3-occupC-c2-empC made responsive

--- web/version.py
+++ web/version.py
@@ -1,12 +1,12 @@
 
-__version__ = "1.2.51"
+__version__ = "1.2.52"
 
-# DBC-row3-occupC 1200px - DBC-row3-occupC-c2-empC made responsive
+# DBC-row3-occupC 992px - DBC-row3-occupC-c2-empC made responsive
 
 # Jedná se o tuto produkční verzi č. ----
 
 # Vytvořil:     Michal Kraninger
-# Datum:        29.6.2026
+# Datum:        30.6.2026
 
 
 
