6fa1686cc5fa6c57873e1276999528bb4dae78b2
Michal <Kraninger.M@seznam.cz>
2026-07-02 09:08:28 +0200

    DBC-row3-lastC-capC4 - 1400px made responsive

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.2.66"
+__version__ = "1.2.67"
 
-# DBC-row3-lastC-capC4 - 992px made responsive
+# DBC-row3-lastC-capC4 - 1400px made responsive
 
 # Jedná se o tuto produkční verzi č. ----
 
