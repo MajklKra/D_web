e2439ab96a308216b1ab3c98d744a2f808e981a6
Michal <Kraninger.M@seznam.cz>
2026-07-07 15:16:16 +0200

    list-patients-component-searching-bar-searchingBox created

---
+++ web/admin/clients/static/js/clients.js
@@ -0,0 +1,29 @@
+
+
+console.log(" %c𝐖𝐄𝐋𝐂𝐎𝐌𝐄 TO client´s page !!! ", "color:yellow");
+
+/* Změna padding pro list-patients-component-searching-bar-searchInput */
+
+
+document.addEventListener("focusin", e =>
+{
+    if (e.target.id === "list-patients-component-searching-bar-searchInput")
+    {
+        document
+            .getElementById("list-patients-component-searchLabel")
+            .classList.add("hidden");
+    }
+});
+
+document.addEventListener("focusout", e =>
+{
+    if (
+        e.target.id === "list-patients-component-searching-bar-searchInput" &&
+        e.target.value === ""
+    )
+    {
+        document
+            .getElementById("list-patients-component-searchLabel")
+            .classList.remove("hidden");
+    }
+});
--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.2.83"
+__version__ = "1.2.84"
 
-# First draft of list of clients page + page container
+# list-patients-component-searching-bar-searchingBox created
 
 # Jedná se o tuto produkční verzi č. ----
 
