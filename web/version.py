0e816d3a282b93b54270c8bbe2d5de93e88b3d15
Michal <Kraninger.M@seznam.cz>
2026-07-09 14:55:12 +0200

    id="list-patients-component-listC-listC2-content-table-box-t1" - icons bor bed added to the table

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.3.02"
+__version__ = "1.3.03"
 
-# id="list-patients-component-listC-listC2-content-table-box-t1 - visualization column 5
+# id="list-patients-component-listC-listC2-content-table-box-t1" - icons bor bed added to the table
 
 # Jedná se o tuto produkční verzi č. ___
 
