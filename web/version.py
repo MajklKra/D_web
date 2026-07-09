94698a0ac7440cfe8bc5b6a0c64a4e188a6ee6b5
Michal <Kraninger.M@seznam.cz>
2026-07-09 14:29:34 +0200

    list-patients-component-listC-listC2-content-table-box-t1 - visualization column 5

--- web/version.py
+++ web/version.py
@@ -1,9 +1,9 @@
 
-__version__ = "1.3.01"
+__version__ = "1.3.02"
 
-# list-patients-component-lessC paging made to be working
+# id="list-patients-component-listC-listC2-content-table-box-t1 - visualization column 5
 
-# Jedná se o tuto produkční verzi č. ----
+# Jedná se o tuto produkční verzi č. ___
 
 # Vytvořil:     Michal Kraninger
 # Datum:        9.7.2026
