50102d08eb8d5b9e3292fa87eb8cccedc6218076
Michal <Kraninger.M@seznam.cz>
2026-07-21 06:39:53 +0200

    client-card-row2-c4-accomC created

--- web/version.py
+++ web/version.py
@@ -1,12 +1,12 @@
 
-__version__ = "1.3.37"
+__version__ = "1.3.38"
 
-# client-card-row2-c3 first attempt
+# client-card-row2-c4-accomC created
 
 # Jedná se o tuto produkční verzi č. ___
 
 # Vytvořil:     Michal Kraninger
-# Datum:        20.7.2026
+# Datum:        21.7.2026
 
 
 
