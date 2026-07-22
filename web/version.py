fb3081e53b092554ad6553e4b4c7fc5390201187
Michal <Kraninger.M@seznam.cz>
2026-07-22 08:56:49 +0200

    .client-card-row2-c4-accomC-occup_patient MODIFIED!

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.3.46"
+__version__ = "1.3.47"
 
-# client-card-row2-c4-accomC-occup_btn hover added
+# .client-card-row2-c4-accomC-occup_patient MODIFIED!
 
 # Jedná se o tuto produkční verzi č. ___
 
