8d9e54f2efe6bb46226de36cb409083a4b59f3a9
Michal <Kraninger.M@seznam.cz>
2026-07-24 08:13:31 +0200

    client-card-row2-c3-SB1C - client-card-row2-c3-SB4C partly functioning 3

--- web/version.py
+++ web/version.py
@@ -1,7 +1,7 @@
 
-__version__ = "1.3.57"
+__version__ = "1.3.58"
 
-# client-card-row2-c3-SB1C - client-card-row2-c3-SB4C partly functioning 2
+# client-card-row2-c3-SB1C - client-card-row2-c3-SB4C partly functioning 3
 
 # Jedná se o tuto produkční verzi č. ___
 
